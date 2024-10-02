cf67ccac470286eab812d8f36f17aab352625317
Phoenixcraft99 <phoenxyz99@gmail.com>
2024-10-01 21:16:40 -0400

    Added settings, moved data
    Moved the data folder for easier access to the read.py module. Added settings for the LUT generator.

--- tools/generate_lut.py
+++ tools/generate_lut.py
@@ -1,1 +1,12 @@
 # Generate phase LUT and CDF-1 LUT
+# Two modes: Droplet mode, molecule mode
+# Metadata:
+# Mode, min and max parameters, molecule data
+# Format: 1800 angles + Absorption cross section + Scattering cross section + Extinction coefficient
+# Changeable format to allow for phase to be phase * cross section * 4pi or phase * absorption cross section, etc.
+
+from .data import read
+
+def initialize_lut_gen():
+    molecule_name = input("Please enter the exact molecule name (str):")
+    read.read_ior(molecule_name)
--- tools/lut_visualize.py
+++ tools/lut_visualize.py
@@ -1,6 +1,1 @@
 # Visualize LUTs on graphs and image form
-# Two modes: Droplet mode, molecule mode
-# Metadata:
-# Mode, min and max parameters, molecule data
-# Format: 1800 angles + Absorption cross section + Scattering cross section + Extinction coefficient
-# Changeable format to allow for phase to be phase * cross section * 4pi or phase * absorption cross section, etc.
